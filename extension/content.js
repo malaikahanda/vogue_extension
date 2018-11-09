0ba231d3b9f3024d9275bb1feb8c560b1daceaef
Malaika Handa <mhanda@andrew.cmu.edu>
2018-11-09 11:39:27 -0500

    icon, more content

--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,8 @@
-alert("Hello from your Chrome extension!")
+// get the first link on the page
+// $ is an alias for the jQuery function (creates an object)
+// .eq selects the 0th element
+// .attr gets the attribute stored at "href"
+var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+// print the output
+console.log(firstHref);
